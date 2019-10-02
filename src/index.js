6aa0fcd31e829ab1ef1d561884566a4fd12311db
AS <ananyavsrivastava@gmail.com>
2019-10-03 00:33:36 +0530

    Something Just Like This

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 //mport react and react dom lib
 import React from 'react';
 import ReactDom from 'react-dom';
-import faker from 'faker';
 
-import CommentDetails from './CommentDetails';
- import ApprovalCard from './ApprovalCard'
 //create a react component
 const App=function(){
     
     return (
-    <div className="ui container comments">
-    <ApprovalCard>
-    <CommentDetails author={faker.name.findName()} timeAgo="Today at 6:35 AM"/>
-    </ApprovalCard>
-    <ApprovalCard>
-    <CommentDetails author={faker.name.findName()} timeAgo="Today at 4:10 PM"/>
-    </ApprovalCard>
-    <ApprovalCard>
-    <CommentDetails author={faker.name.findName()} timeAgo="Yesterday at 9:00 PM"/>
-    </ApprovalCard>
-    </div>
+    
     );
 };
 
